d2740461de38efcdc52bc15db041fa57446f2bb6
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-07-19 01:28:16 +0530

    first commit

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("API Handling"),
+      ),
+      body: SafeArea(child: ListView(
+
+      )),
+    );
+  }
+}
